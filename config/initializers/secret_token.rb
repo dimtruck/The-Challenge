8847008c228914ce5fb5b80627e7e21d9ff0450b
Dimitry Ushakov <dima@gympilot.com>
2012-04-18 09:29:18 -0500

    update git

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TheChallenge::Application.config.secret_token = 'db20e79039a877be478b588cd67ccb54907b71deb24afff7c4c95427a734b06b9fd42ff6333cb7a2ec2cf36d3d6e9570e002dbe5a1e4a03af60761562d07190d'
